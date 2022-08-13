1e4c13bc21fc4547dae2b644efe55e155382f6b1
David Lambert <david.lambert@my.utsa.edu>
2022-08-13 18:57:09 -0500

    finished panagram

--- panagram.js
+++ panagram.js
@@ -1,8 +1,17 @@
 
-const alphabet = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
 function isPanagram(sentence){
+    sentence = sentence.toLowerCase()
+    found = true
     for (let char of alphabet){
-        if (!sentence.includes(char))
+        if (!sentence.includes(char)){
+            found = false
+            break
+        }
     }
+    console.log(found)
 }
+
+isPanagram("The five boxing wizards jump quickly") //true
+isPanagram("the five boxing wizards jump quick") //false
