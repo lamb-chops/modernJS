3fd37627ac3174152eaedd908790907715406abe
David Lambert <david.lambert@my.utsa.edu>
2022-08-14 09:34:03 -0500

    added html file for debugging through dev tools

--- getCard.js
+++ getCard.js
@@ -1,4 +1,16 @@
 
 function getCard() {
-    
+    suit = Math.ceil(Math.random() * 4) //1= clubs, 2=spades, 3=hearts, 4=diamonds
+    if ( suit = 1){
+        suit = "C"
+    }
+    else if ( suit = 2){
+        suit = "S"
+    }
+    else if ( suit = 3 ){
+        suit = "H"
+    }
+    else suit = 'D'
+
+    console.log(suit)
 }
