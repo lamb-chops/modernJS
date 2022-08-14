4a75a0dda81c3706c1e49bd476794acecf9d7567
David Lambert <david.lambert@my.utsa.edu>
2022-08-14 12:37:53 -0500

    finished getCard.js

--- getCard.js
+++ getCard.js
@@ -1,19 +1,33 @@
 
 function getCard() {
 
-    suit = Math.ceil(Math.random() * 4) //1= clubs, 2=spades, 3=hearts, 4=diamonds
-    if ( suit = 1){
+    suit = Math.floor(Math.random() * 4) //1= clubs, 2=spades, 3=hearts, 4=diamonds
+    if ( suit == 1){
         suit = "C"
     }
-    else if ( suit = 2){
+    else if ( suit == 2){
         suit = "S"
     }
-    else if ( suit = 3 ){
+    else if ( suit == 3 ){
         suit = "H"
     }
     else suit = 'D'
 
-    console.log(suit)
+    const cards = '2 3 4 5 6 7 8 9 J Q K A'
+    const cardsArray = cards.split(" ")
+    let cardNum = Math.floor(Math.random() * cardsArray.length);
+    let returnCard = cardsArray[cardNum]
+
+    const returnObject = {
+        suitSelected: suit,
+        cardSelected: returnCard
+    }
+    return returnObject
 }
 
-alert("its working")
+console.log(getCard())
+console.log(getCard())
+console.log(getCard())
+console.log(getCard())
+console.log(getCard())
+console.log(getCard())
